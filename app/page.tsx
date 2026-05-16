5f591e38c221fc0e6065defb4be3a2d54b21838e
Kheireditz <miftahulkhairim1@gmail.com>
2026-05-17 05:55:03 +0700

    Add Home component for Ecommerce System

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Ecommerce System Ready</h1>
+      <p>Login + Products + Cart + History + Admin</p>
+    </div>
+  );
+}
